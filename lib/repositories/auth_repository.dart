b73c7e89d512a2ba6b7b65222353dfb8882e721a
Ebrahim <ebrahimahmed804853@gmail.com>
2025-08-15 17:25:50 +0300

    auth repository added

---
+++ lib/repositories/auth_repository.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepository {
+  final FirebaseAuth _auth;
+  final FirebaseFirestore _firestore;
+
+  AuthRepository(this._auth, this._firestore);
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  // Sign up with email and password
+  Future<UserCredential> signUp({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    final userCredential = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    // Save user info to Firestore
+    await _firestore.collection('users').doc(userCredential.user!.uid).set({
+      'name': name,
+      'email': email,
+      'photoUrl': '',
+      'createdAt': FieldValue.serverTimestamp(),
+    });
+
+    return userCredential;
+  }
+
+  // Login with email and password
+  Future<UserCredential> signIn({
+    required String email,
+    required String password,
+  }) async {
+    return await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  // Logout
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}

6d7c01a2bf1d536d246c961de27bba08adc9698b
Ebrahim <ebrahimahmed804853@gmail.com>
2025-08-18 23:32:57 +0300

    result screen updated

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quiz_app/screens/quiz_screen.dart';
+import 'package:quiz_app/screens/splash_screen.dart';
+import 'package:quiz_app/theme/app_background.dart';
 import '../models/category_model.dart';
 
 class ResultScreen extends ConsumerWidget {
@@ -18,72 +21,97 @@ class ResultScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final percent = (score / totalQuestions);
 
-    return Scaffold(
-      appBar: AppBar(title: const Text("Quiz Result"), centerTitle: true),
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              category.name,
-              style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 24),
+    return AppBackground(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Theme.of(context).colorScheme.onPrimaryFixedVariant,
+          title: Text(
+            "Quiz Result",
+            style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                category.name,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 24),
 
-            // Circular percentage
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                SizedBox(
-                  width: 160,
-                  height: 160,
-                  child: CircularProgressIndicator(
-                    value: percent,
-                    strokeWidth: 10,
-                    backgroundColor: Colors.grey.shade300,
-                    color: percent >= 0.5 ? Colors.green : Colors.red,
+              // Circular percentage
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  SizedBox(
+                    width: 160,
+                    height: 160,
+                    child: CircularProgressIndicator(
+                      value: percent,
+                      strokeWidth: 10,
+                      backgroundColor: Colors.grey.shade300,
+                      color: percent >= 0.5 ? Colors.green : Colors.red,
+                    ),
                   ),
-                ),
-                Text(
-                  "${(percent * 100).toStringAsFixed(0)}%",
-                  style: const TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
+                  Text(
+                    "${(percent * 100).toStringAsFixed(0)}%",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
                   ),
-                ),
-              ],
-            ),
+                ],
+              ),
 
-            const SizedBox(height: 24),
-            Text(
-              "Score: $score / $totalQuestions",
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 32),
+              SizedBox(height: 24),
+              Text(
+                "Score: $score / $totalQuestions",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+              ),
+              SizedBox(height: 32),
 
-            // Buttons
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pop(context); // close result
-                    Navigator.pop(context); // back to categories
-                  },
-                  icon: const Icon(Icons.home),
-                  label: const Text("Back"),
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pop(context); // close result
-                  },
-                  icon: const Icon(Icons.refresh),
-                  label: const Text("Retry"),
-                ),
-              ],
-            ),
-          ],
+              // Buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (_) => SplashScreen()),
+                      );
+                    },
+                    icon: const Icon(Icons.home),
+                    label: const Text("Back"),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => QuizScreen(category: category),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.refresh),
+                    label: const Text("Retry"),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
